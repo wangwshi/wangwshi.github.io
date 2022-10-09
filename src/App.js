9fa0cf67488384d44588ca50062d8d637f1a9757
wangwshi <wwang912247@gmail.com>
2022-10-08 22:47:05 -0500

    testing catch-all

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Router>
           <Navbar/>
           <Routes>
-            <Route path="/" element={<About/>} />
+            <Route path="/*" element={<About/>} />
             <Route path="/wangwshi.github.io" element={<About/>} />
             <Route path="/about" element={<About/>} />
             <Route path="/art" element={<Art/>} />
